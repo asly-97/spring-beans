dedb001304022c80d75147945936c9a0e6518df9
Abdeljabbar Elassali <115737234+asly-97@users.noreply.github.com>
2024-07-24 22:36:45 -0400

    init

---
+++ src/test/java/com/revature/spring_beans/SpringBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.spring_beans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
